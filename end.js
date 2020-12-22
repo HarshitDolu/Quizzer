256d116bacc1189aa00a03a9b11915d80c3771a2
Harshit Jain <51118591+HarshitDolu@users.noreply.github.com>
2020-12-22 11:42:09 +0530

    Add files via upload

--- end.js
+++ end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/project6/');
+    window.location.assign('/');
 };
